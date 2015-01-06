6b057ecb219483b9385dea536e28058867c7e94b
Vali Malinoiu <0x4139@gmail.com>
2015-01-06 15:48:22 +0200

    bigger database and improved hashing algorithm NOT

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 var DB=require('./db');
 var DBCount=DB.length;
-
-var hashString = function(str) {
+var hashString = function(str){
     var hash = 0;
-    if (str.length === 0) return hash;
     for (i = 0; i < str.length; i++) {
         char = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32bit integer
-        return hash;
+        hash += char;
     }
+    return hash;
 };
 module.exports=function(str){
 	var hash=hashString(str);
+	console.log(hash);
 	return DB[hash%DBCount];
 };
--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 var FakeAgent=require('./');
-console.log(FakeAgent('0x4139.com'));
+console.log(FakeAgent('0x4139@gmail.com'));
